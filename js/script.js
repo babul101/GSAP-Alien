8b34de641ab6d6aa5e8f07481b0f04d064b4188e
babul101 <babuldeb101@gmail.com>
2024-03-12 15:54:39 +0530

    animating my hat with gsap

--- js/script.js
+++ js/script.js
@@ -7,6 +7,17 @@ const eyeballs = document.querySelectorAll(
 );
 
 eyesTimeline
-	.set(eyeballs, { y: 0 })
-	.to(eyeballs, { y: 7, duration: 0.25, delay: 2, stagger: 0.25 })
-	.to(eyeballs, { y: 0, duration: 0.25, delay: 4 })
+  .set(eyeballs, { y: 0 })
+  .to(eyeballs, { y: 7, duration: 0.25, delay: 2, stagger: 0.25 })
+  .to(eyeballs, { y: 0, duration: 0.25, delay: 4 });
+
+const hatTimeline = gsap.timeline({
+  repeat: -1,
+  repeatDelay: 4,
+});
+
+const hat = document.querySelector("g#hat")
+hatTimeline
+	.set(hat, { y: 0 })
+	.to(hat, { y: -50, rotation: -10, duration: 0.25, delay: 1 })
+	.to(hat, { y: 0, rotation: 0, duration: 0.5, delay: 0.1 })
